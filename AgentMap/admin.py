38b8dd428e38aca93670a2dcf4776b5598560b22
ASaltyStik <Carickbrandt@gmail.com>
2024-03-21 12:18:47 -0400

    Added Agent + LicensedState Models

--- AgentMap/admin.py
+++ AgentMap/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 
-from AgentMap.models import Form
+from AgentMap.models import Agent, Form, LicensedState
 
 
 # Register your models here.
 @admin.register(Form)
 class FormAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Agent)
+class AgentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(LicensedState)
+class LicensedStateAdmin(admin.ModelAdmin):
     pass
