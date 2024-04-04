f622701ffdadb56d368cb97118b7ed8e55a793bb
ASaltyStik <Carickbrandt@gmail.com>
2024-04-04 17:10:51 -0400

    Comments

--- AgentMap/forms.py
+++ AgentMap/forms.py
@@ -3,11 +3,13 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 
+# Form for the login page
 class LoginForm(forms.Form):
     username = forms.CharField(label='Username', max_length=100)
     password = forms.CharField(label='Password', widget=forms.PasswordInput)
 
 
+# Form for the user registration page
 class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField(label='Email')
 
