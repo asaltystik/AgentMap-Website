92cde30e8d8a52c031cd461a96f645526a56341d
ASaltyStik <Carickbrandt@gmail.com>
2024-04-04 15:33:09 -0400

    Refactoring names to be more human-readable

--- AgentMap/management/commands/warn_expiration.py
+++ AgentMap/management/commands/warn_expiration.py
@@ -13,11 +13,11 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         # Get the current date and the last day of the current month
         current_date = timezone.now().date()
-        date_31_days_away = current_date + timedelta(days=31)  # Doing 31 days to cover the 7 months with 31 days.
+        end_date = current_date + timedelta(days=31)  # Doing 31 days to cover the 7 months with 31 days.
         # Ignoring the case of a leap year cause the next one is 4 years away.
 
         # Query the LicensedState model for any licenses expiring within the current month
-        expiring_licenses = LicensedState.objects.filter(expiration__range=(current_date, date_31_days_away))
+        expiring_licenses = LicensedState.objects.filter(expiration__range=(current_date, end_date))
 
         # Group the results by agent
         agents = expiring_licenses.values('agent').annotate(num_expiring=Count('id'))
@@ -26,10 +26,10 @@ class Command(BaseCommand):
         for agent in agents:
             agent_obj = Agent.objects.get(id=agent['agent'])
             agent_expirations = expiring_licenses.filter(agent=agent_obj)
-            expirations_str = "\n".join([f"License {exp.licenseNumber}"
-                                         f" in state {exp.state}"
-                                         f" expires on {exp.expiration.strftime('%m/%d/%Y')}"
-                                         for exp in agent_expirations])
+            expirations_str = "\n".join([f"License {expiring_license.licenseNumber}"
+                                         f" in state {expiring_license.state}"
+                                         f" expires on {expiring_license.expiration.strftime('%m/%d/%Y')}"
+                                         for expiring_license in agent_expirations])
             send_mail(
                 'License Expiration Notice',
                 f'You have {agent["num_expiring"]} license(s) expiring'
