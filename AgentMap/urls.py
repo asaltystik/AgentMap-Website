25477be8e83292cdae3ab72403cdf0da2cdef909
ASaltyStik <Carickbrandt@gmail.com>
2024-04-05 10:54:31 -0400

    Comments

--- AgentMap/urls.py
+++ AgentMap/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 
+
+# URL patterns for the AgentMap web application
 urlpatterns = [
-    path('login/', views.login_view, name='Login'),
-    path('logout/', views.logout_view, name='Logout'),
-    path('register/', views.register_view, name='Register'),
-    path('', views.agent_map, name='AgentMap'),
+    path('login/', views.login_view, name='Login'),  # URL Pattern for the login page
+    path('logout/', views.logout_view, name='Logout'),  # URL Pattern for the logout page
+    path('register/', views.register_view, name='Register'),  # URL Pattern for the register page
+    path('', views.agent_map, name='AgentMap'),  # URL Pattern for the agent map page
 ]
