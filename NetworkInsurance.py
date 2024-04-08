c6742601a92e190725c62c51ba8ba1437f5c8f99
ASaltyStik <Carickbrandt@gmail.com>
2024-04-08 10:14:16 -0400

    Added Comments

--- NetworkInsurance.py
+++ NetworkInsurance.py
@@ -13,15 +13,15 @@ def scrape():
             # if the line is all caps it is a product name
             if line.isupper():
                 current_product = line
-                product_list[current_product] = []
+                product_list[current_product] = []  # Add the product to the product list
                 # print("Product: " + line)
             # if the line is not all caps, it is a company name
             elif current_product is not None:
-                product_list[current_product].append(line)
+                product_list[current_product].append(line)  # Append the company to the product list
                 # print("Company" + line)
         print(product_list)
         # fix product_list to be a json object
-        product_list = str(product_list).replace("'", "\"")
+        product_list = str(product_list).replace("'", "\"")  # Replace single quatations with double quotations
         # Save the product list to a file
         with open("C:\\Users\\Noricum\\Downloads\\NetworkInsuranceCarriers.json", "w") as output:
             output.write(str(product_list))
