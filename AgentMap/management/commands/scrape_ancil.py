1109145a9b813916f3fb3c6dbc019d1260586ee7
ASaltyStik <Carickbrandt@gmail.com>
2024-04-17 12:35:00 -0400

    refactored NetworkInsurance.py into scrape_ancil.py which is now a management command

---
+++ AgentMap/management/commands/scrape_ancil.py
@@ -0,0 +1,34 @@
+from django.core.management.base import BaseCommand
+import os
+
+
+# This function will loop through line by line to get the Product name and relevant companies
+# Honestly, Might be better to Deprecate this function, Dont know if it is still
+# Useful to the project
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('file_path', type=str)
+        return 0
+
+    def handle(self, *args, **options):
+        file = options['file_path']
+        # Open the pdf
+        with open(file) as file:
+            product_list = {}
+            current_product = ""
+            # loop through the lines
+            for line in file:
+                line = line.strip()
+                # if the line is all caps it is a product name
+                if line.isupper():
+                    current_product = line
+                    product_list[current_product] = []
+                elif current_product is not None:
+                    product_list[current_product].append(line)
+            print(product_list)
+            product_list = str(product_list).replace("'", "\"")
+            # Save the product list to a file in static dir
+            with open(os.path.join(os.getcwd(), 'static', 'NetworkInsuranceCarriers.json'), "w") as output:
+                output.write(str(product_list))
+                print("File Saved")
+        return 0
