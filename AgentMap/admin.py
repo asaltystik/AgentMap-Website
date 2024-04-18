f363fd50abb5a1385abad53e9a6cc13eba806dfe
ASaltyStik <Carickbrandt@gmail.com>
2024-04-18 14:39:23 -0400

    Spicy comments

--- AgentMap/admin.py
+++ AgentMap/admin.py
@@ -3,17 +3,19 @@ from django.contrib import admin
 from AgentMap.models import Agent, Form, LicensedState
 
 
-# Registration of the models in the admin site
+# Registration of the Form model in the admin site
 @admin.register(Form)
 class FormAdmin(admin.ModelAdmin):
     pass
 
 
+# Registration of the Agent model in the admin site
 @admin.register(Agent)
 class AgentAdmin(admin.ModelAdmin):
     pass
 
 
+# Registration of the LicensedState model in the admin site
 @admin.register(LicensedState)
 class LicensedStateAdmin(admin.ModelAdmin):
     pass
