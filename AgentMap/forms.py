efd627b64aacece62882479da1061ce648d49630
ASaltyStik <Carickbrandt@gmail.com>
2024-07-26 14:46:04 -0400

    added in autocomplete tags into the login form cause google just yelled at me when i hit sign out. /shrug

--- AgentMap/forms.py
+++ AgentMap/forms.py
@@ -5,8 +5,15 @@ from django.contrib.auth.forms import UserCreationForm
 
 # Form for the login page
 class LoginForm(forms.Form):
-    username = forms.CharField(label='Username', max_length=100)
-    password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    username = forms.CharField(
+        label='Username',
+        max_length=100,
+        widget=forms.TextInput(attrs={'autocomplete': 'username'})
+    )
+    password = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput(attrs={'autocomplete': 'current-password'})
+    )
 
 
 # Form for the user registration page
